50d855b6623c057380bbfc0992228ae0e0a7987f
lukasvi <58526151+lukasvi@users.noreply.github.com>
2025-02-23 20:44:32 +0100

    Extract generator report logic to own class

---
+++ GermanToCSharpKeywordsGenerator/DiagnosticsReporter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+
+namespace GermanToCSharpKeywordsGenerator;
+
+public static class DiagnosticsReporter
+{
+    public static void Report(GeneratorExecutionContext context, SyntaxNode root, string filePath)
+    {
+        var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+
+        if (classDeclarations.Count == 0)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(
+                GeneratorDiagnostics.ClassNotFoundErrorDescriptor,
+                Location.Create(filePath, new TextSpan(0, 0), new LinePositionSpan(new LinePosition(0, 0), new LinePosition(0, 0))),
+                filePath
+            ));
+            return;
+        }
+
+        if (classDeclarations.Count > 1)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(
+                descriptor: GeneratorDiagnostics.MultipleClassesErrorDescriptor,
+                location: classDeclarations[0].GetLocation(),
+                additionalLocations: classDeclarations.Skip(1).Select(x => x.GetLocation()),
+                filePath
+            ));
+        }
+
+        var expectedClassName = Path.GetFileNameWithoutExtension(filePath);
+        var classDeclaration = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First();
+        if (!classDeclaration.Identifier.Text.Equals(expectedClassName, StringComparison.OrdinalIgnoreCase))
+        {
+            context.ReportDiagnostic(Diagnostic.Create(
+                GeneratorDiagnostics.ClassNameMismatchErrorDescriptor,
+                classDeclaration.Identifier.GetLocation(),
+                classDeclaration.Identifier.Text, expectedClassName
+            ));
+        }
+
+        var namespaceDeclaration = root.DescendantNodes().OfType<NamespaceDeclarationSyntax>().FirstOrDefault();
+
+        var fileScopednamespaceDeclaration = root.DescendantNodes().OfType<FileScopedNamespaceDeclarationSyntax>().FirstOrDefault();
+
+
+        if (namespaceDeclaration == null && fileScopednamespaceDeclaration == null)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(
+                GeneratorDiagnostics.NamespaceNotFoundErrorDescriptor,
+                Location.Create(filePath, new TextSpan(), new LinePositionSpan()),
+                filePath
+            ));
+            return;
+        }
+    }
+
+    public static void ReportInternalError(GeneratorExecutionContext context, Exception ex)
+    {
+        context.ReportDiagnostic(Diagnostic.Create(GeneratorDiagnostics.GeneralErrorDescriptor, Location.None, ex.Message));
+    }
+}
---
+++ GermanToCSharpKeywordsGenerator/GeneratorDiagnostics.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GermanToCSharpKeywordsGenerator;
+
+public static class GeneratorDiagnostics
+{
+
+    public static readonly DiagnosticDescriptor GeneralErrorDescriptor = new(
+        id: "GEN001",
+        title: "General Source Generator Execution Error",
+        messageFormat: "An error occurred during source generator execution: {0}",
+        category: "Generator",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor ClassNotFoundErrorDescriptor = new(
+        id: "GEN002",
+        title: "Source Generator Execution Error: Class not found",
+        messageFormat: "The classname was not found in file: {0}",
+        category: "Generator",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor NamespaceNotFoundErrorDescriptor = new(
+        id: "GEN003",
+        title: "Source Generator Execution Error: Namespace not found",
+        messageFormat: "No namespace was found in file: {0}",
+        category: "Generator",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor MultipleClassesErrorDescriptor = new(
+        id: "GEN004",
+        title: "Source Generator Execution Warning: Multiple classes found",
+        messageFormat: "More than one class was found in file: {0}",
+        category: "Generator",
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor ClassNameMismatchErrorDescriptor = new(
+        id: "GEN005",
+        title: "Source Generator Execution Warning: Class name does not match file name",
+        messageFormat: "The class name '{0}' does not match the file name '{1}'",
+        category: "Generator",
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true
+    );
+}
